b60385180c3355129797e303d4d6eb8741e30715
IsaiasS1985 <111474163+IsaiasS1985@users.noreply.github.com>
2023-04-25 17:54:31 -0300

    actualizo la clase ventana

--- TP4_GRUPO_7/src/Main/Ventana.java
+++ TP4_GRUPO_7/src/Main/Ventana.java
@@ -1,73 +1,71 @@
 package Main;
 
-
-
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Ventana extends JFrame{
-	
+public class Ventana extends JFrame {
+
 	private static final long serialVersionUID = 1L;
-	
-	private JButton boton1, boton2, boton3;
-	
+	private JPanel contentPane;
+	private JButton btnEjercicio;
+	private JButton btnEjercicio_1;
+	private JButton btnEjercicio_2;
+	private JLabel lblNewLabel;
+
 	public Ventana() {
-		
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500,500);
-		setLocation(350,350);
-		setTitle("Trabajo Practico Nº4");
-		setLayout(null);
+		setBounds(100, 100, 450, 300);
+		setTitle("Trabajo Practico N°4");
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		// BOTON 1
-		boton1 = new JButton();
-		boton1.addActionListener(new ActionListener() {
+		btnEjercicio = new JButton("EJERCICIO 1");
+		btnEjercicio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Ejercicio1 v = new Ejercicio1();
 				v.setVisible(true);
 			}
 		});
-		boton1.setText("Ejercicio 1");
-		boton1.setBounds(20,25,140,30);
-		getContentPane().add(boton1);
-		setLayout(null);
+		btnEjercicio.setBounds(163, 82, 108, 33);
+		contentPane.add(btnEjercicio);
 		
-		
-		// BOTON 2
-		boton2 = new JButton();
-		boton2.addActionListener(new ActionListener() {
+		btnEjercicio_1 = new JButton("EJERCICIO 2");
+		btnEjercicio_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Ejercicio2 v = new Ejercicio2();
 				v.setVisible(true);
 			}
 		});
-		boton2.setText("Ejercicio 2");
-		boton2.setBounds(20,75,140,30);
-		getContentPane().add(boton2);
+		btnEjercicio_1.setBounds(163, 131, 108, 33);
+		contentPane.add(btnEjercicio_1);
 		
-		// BOTON 3
-		boton3 = new JButton();
-		boton3.addActionListener(new ActionListener() {
+		btnEjercicio_2 = new JButton("EJERCICIO 3");
+		btnEjercicio_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Ejercicio3 v = new Ejercicio3();
 				v.setVisible(true);
 			}
 		});
-		boton3.setText("Ejercicio 3");
-		boton3.setBounds(20,125,140,30);
-		getContentPane().add(boton3);
+		btnEjercicio_2.setBounds(163, 184, 108, 33);
+		contentPane.add(btnEjercicio_2);
 		
-		
-		
+		lblNewLabel = new JLabel("GRUPO NRO: 7");
+		lblNewLabel.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 18));
+		lblNewLabel.setBounds(31, 29, 166, 33);
+		contentPane.add(lblNewLabel);
 	}
 	
-	public void cambiarVisibilidad(boolean estado) {
+	public void cambiarVisibilidad(boolean estado)
+	{
 		setVisible(true);
 	}
-	
-	
+
 }
-
-
